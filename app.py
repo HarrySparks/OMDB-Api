07e913e577bd74deaa75f377304e14ddc37137ab
HarrySparks <700369@student.cityofbristol.ac.uk>
2023-03-30 09:06:31 +0000

    Set Up

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request 
+
+import requests 
+
+app = Flask(__name__) 
+
+@app.route('/', methods=['GET', 'POST']) 
+
+def index(): 
+    movies = [] 
+ 
+    if request.method == 'POST': 
+        search_query = request.form['search_query'] 
+        api_key = "174d12c6" 
+        url = f"http://www.omdbapi.com/?s={search_query}&apikey={api_key}" 
+        response = requests.get(url) 
+        data = response.json() 
+        print(data) #look in the terminal at the data 
+    return render_template('index.html', movies=movies) 
+
+ 
+ 
+
+if __name__ == "__main__": 
+    app.run(debug=True)
