75bef47951958f374bfe8a32829f615aaf384e2d
HarrySparks <700369@student.cityofbristol.ac.uk>
2023-03-30 09:48:18 +0000

    Image

--- app.py
+++ app.py
@@ -11,15 +11,21 @@ def index():
  
     if request.method == 'POST': 
         search_query = request.form['search_query'] 
-        api_key = "174d12c6" 
+        api_key = "f0674f3f" 
         url = f"http://www.omdbapi.com/?s={search_query}&apikey={api_key}" 
         response = requests.get(url) 
         data = response.json() 
         print(data) #look in the terminal at the data 
-    return render_template('index.html', movies=movies) 
 
- 
- 
+        #If the response is true from the JSON then send the movies to the webpage... 
+        if data.get('Response') == 'True': 
+            movies = data["Search"] # this gets the array of movies using the key Search 
+            return render_template('index.html', movies=movies) 
+        else: 
+            print("Bad Response!") 
+
+
+    return render_template('index.html', movies=movies) 
 
 if __name__ == "__main__": 
     app.run(debug=True)
